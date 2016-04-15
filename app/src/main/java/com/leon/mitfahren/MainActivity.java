59cf081183cb674fe3b650037ced8865a5a0d300
LeonBre <Leon@MacBook-Air.local>
2016-04-15 15:24:33 +0200

    Implented Date and Time Picker
    Beautiful Commentary

--- app/src/main/java/com/leon/mitfahren/MainActivity.java
+++ app/src/main/java/com/leon/mitfahren/MainActivity.java
@@ -1,15 +1,88 @@
 package com.leon.mitfahren;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button dateButton;
+    Button timeButton;
+    Calendar myCalendar = Calendar.getInstance();
+
+    DatePickerDialog.OnDateSetListener date;
+    TimePickerDialog.OnTimeSetListener time;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
-    }
-    
 
+        dateButton = (Button) findViewById(R.id.buttonDatum);
+        timeButton = (Button) findViewById(R.id.buttonZeit);
+
+        date = new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateDateLabel();
+            }
+        };
+
+        time = new TimePickerDialog.OnTimeSetListener(){
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                myCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                myCalendar.set(Calendar.MINUTE, minute);
+                updateTimeLabel();
+            }
+        };
+
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog mDialog = new DatePickerDialog(MainActivity.this, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH));
+                mDialog.show();
+
+            }
+        });
+
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //new TimePicker
+                new TimePickerDialog(MainActivity.this, time, myCalendar.get(Calendar.HOUR_OF_DAY), myCalendar.get(Calendar.MINUTE), true).show();
+            }
+        });
+    }
+
+    private void updateDateLabel() {
+        String myFormat = "MM/dd/yy"; // In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        dateButton.setText(sdf.format(myCalendar.getTime()));
+        // dateButton.setFreezesText(true);
+    }
+
+    private void updateTimeLabel() {
+        String myFormat = "H:mm";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        timeButton.setText(sdf.format(myCalendar.getTime()));
+
+    }
 }
