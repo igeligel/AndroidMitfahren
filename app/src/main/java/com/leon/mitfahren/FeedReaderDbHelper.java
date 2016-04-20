bc6bf41ccc6ff981ede23c4659e03920f82249ea
igeligel <kevinigeligeligel@gmail.com>
2016-04-20 20:07:52 +0200

    Checkstyle + autocomplete
    Added Checkstyle + autocomplete for first field in search activity.

--- app/src/main/java/com/leon/mitfahren/FeedReaderDbHelper.java
+++ app/src/main/java/com/leon/mitfahren/FeedReaderDbHelper.java
@@ -1,6 +1,6 @@
 package com.leon.mitfahren;
 
-import com.leon.mitfahren.FeedReaderContract.*;
+// import com.leon.mitfahren.FeedReaderContract.*;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
@@ -10,41 +10,53 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by Leon on 20.04.16.
  */
 public class FeedReaderDbHelper extends SQLiteOpenHelper {
-    // If you change the database schema, you must increment the database version.
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "drive.db";
+  // If you change the database schema, you must increment the database version.
 
-    private static final String TEXT_TYPE = " TEXT";
-    private static final String INTEGER_TYPE = " INTEGER";
-    private static final String COMMA_SEP = ",";
-    private static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
-                    FeedEntry.COLUMN_NAME_ENTRY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-                    FeedEntry.COLUMN_NAME_YEAR + INTEGER_TYPE + COMMA_SEP +
-                    FeedEntry.COLUMN_NAME_MONTH + INTEGER_TYPE + COMMA_SEP +
-                    FeedEntry.COLUMN_NAME_DAY + INTEGER_TYPE + COMMA_SEP +
-                    FeedEntry.COLUMN_NAME_HOUR + INTEGER_TYPE + COMMA_SEP +
-                    FeedEntry.COLUMN_NAME_MINUTE + INTEGER_TYPE + COMMA_SEP +
-                    FeedEntry.COLUMN_NAME_FROM + TEXT_TYPE + COMMA_SEP +
-                    FeedEntry.COLUMN_NAME_TO + TEXT_TYPE +
-                    " );";
+  public static final int DATABASE_VERSION = 1;
+  public static final String DATABASE_NAME = "drive.db";
 
-    private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
 
-    public FeedReaderDbHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(SQL_CREATE_ENTRIES);
-    }
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // This database is only a cache for online data, so its upgrade policy is
-        // to simply to discard the data and start over
-        db.execSQL(SQL_DELETE_ENTRIES);
-        onCreate(db);
-    }
-    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        onUpgrade(db, oldVersion, newVersion);
-    }
+  private static final String TEXT_TYPE = " TEXT";
+  private static final String INTEGER_TYPE = " INTEGER";
+  private static final String COMMA_SEP = ",";
+  private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + FeedReaderContract.FeedEntry.TABLE_NAME + " ("
+              + FeedReaderContract.FeedEntry.COLUMN_NAME_ENTRY_ID
+              + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+              + FeedReaderContract.FeedEntry.COLUMN_NAME_YEAR + INTEGER_TYPE + COMMA_SEP
+              + FeedReaderContract.FeedEntry.COLUMN_NAME_MONTH + INTEGER_TYPE + COMMA_SEP
+              + FeedReaderContract.FeedEntry.COLUMN_NAME_DAY + INTEGER_TYPE + COMMA_SEP
+              + FeedReaderContract.FeedEntry.COLUMN_NAME_HOUR + INTEGER_TYPE + COMMA_SEP
+              + FeedReaderContract.FeedEntry.COLUMN_NAME_MINUTE + INTEGER_TYPE + COMMA_SEP
+              + FeedReaderContract.FeedEntry.COLUMN_NAME_FROM + TEXT_TYPE + COMMA_SEP
+              + FeedReaderContract.FeedEntry.COLUMN_NAME_TO + TEXT_TYPE
+              + " );";
+
+  private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + FeedReaderContract.FeedEntry.TABLE_NAME;
+
+  public FeedReaderDbHelper(Context context) {
+    super(context, DATABASE_NAME, null, DATABASE_VERSION);
+  }
+
+  public void onCreate(SQLiteDatabase db) {
+    db.execSQL(SQL_CREATE_ENTRIES);
+  }
+
+  /**
+   * Method to upgrade the database.
+   * @param db Database to update.
+   * @param oldVersion OldVersion.
+   * @param newVersion NewVersion.
+   */
+  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    // This database is only a cache for online data, so its upgrade policy is
+    // to simply to discard the data and start over
+    db.execSQL(SQL_DELETE_ENTRIES);
+    onCreate(db);
+  }
+
+  public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    onUpgrade(db, oldVersion, newVersion);
+  }
 }
--- app/src/main/java/com/leon/mitfahren/MainActivity.java
+++ app/src/main/java/com/leon/mitfahren/MainActivity.java
@@ -4,156 +4,186 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.res.Configuration;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.os.PersistableBundle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
-import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-import com.leon.mitfahren.FeedReaderContract.*;
+// import com.leon.mitfahren.FeedReaderContract;
 
 public class MainActivity extends AppCompatActivity {
+  Button dateButton;
+  Button timeButton;
+  Calendar myCalendar = Calendar.getInstance();
 
-    Button dateButton;
-    Button timeButton;
-    Calendar myCalendar = Calendar.getInstance();
+  DatePickerDialog.OnDateSetListener date;
+  TimePickerDialog.OnTimeSetListener time;
 
+  @Override
+  protected void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.search);
+    // Get a reference to the AutoCompleteTextView in the layout
+    AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.editTextVon);
+    // Get the string array
+    String[] countries = getResources().getStringArray(R.array.cities_array);
+    // Create the adapter and set it to the AutoCompleteTextView
+    ArrayAdapter<String> adapter =
+        new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries);
+    textView.setAdapter(adapter);
+    dateButton = (Button) findViewById(R.id.buttonDatum);
+    timeButton = (Button) findViewById(R.id.buttonZeit);
 
-    DatePickerDialog.OnDateSetListener date;
-    TimePickerDialog.OnTimeSetListener time;
-
-    @Override
-    protected void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.search);
-
-        dateButton = (Button) findViewById(R.id.buttonDatum);
-        timeButton = (Button) findViewById(R.id.buttonZeit);
-
-        date = new DatePickerDialog.OnDateSetListener(){
-            @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                myCalendar.set(Calendar.YEAR, year);
-                myCalendar.set(Calendar.MONTH, monthOfYear);
-                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateDateLabel();
-            }
-        };
-
-        time = new TimePickerDialog.OnTimeSetListener(){
-            @Override
-            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                myCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
-                myCalendar.set(Calendar.MINUTE, minute);
-                updateTimeLabel();
-            }
-        };
-
-        dateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DatePickerDialog mDialog = new DatePickerDialog(MainActivity.this, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH));
-                mDialog.show();
-
-            }
-        });
-
-        timeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //new TimePicker
-                new TimePickerDialog(MainActivity.this, time, myCalendar.get(Calendar.HOUR_OF_DAY), myCalendar.get(Calendar.MINUTE), true).show();
-            }
-        });
-    }
-
-    private void updateDateLabel() {
-        String myFormat = "MM/dd/yy"; // In which you need put here
-        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-        dateButton.setText(sdf.format(myCalendar.getTime()));
-        // dateButton.setFreezesText(true);
-    }
-
-    private void updateTimeLabel() {
-        String myFormat = "H:mm";
-        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-        timeButton.setText(sdf.format(myCalendar.getTime()));
-
-    }
-
-    public void saveData(View view) {
-        EditText vonText = (EditText) findViewById(R.id.editTextVon);
-        EditText nachText = (EditText) findViewById(R.id.editTextNach);
-
-        String fromString = vonText.getText().toString();
-        String toString = nachText.getText().toString();
-
-        if(fromString != "" && toString != ""){
-            int year = myCalendar.get(Calendar.YEAR);
-            int month = myCalendar.get(Calendar.MONTH);
-            int day = myCalendar.get(Calendar.DAY_OF_MONTH);
-            int hour = myCalendar.get(Calendar.HOUR_OF_DAY);
-            int minute = myCalendar.get(Calendar.MINUTE);
-
-            saveData(year, month, day, hour, minute, fromString, toString);
-            loadData();
+    date = new DatePickerDialog.OnDateSetListener() {
+      @Override
+      public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        myCalendar.set(Calendar.YEAR, year);
+        myCalendar.set(Calendar.MONTH, monthOfYear);
+        myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        updateDateLabel();
         }
+    };
+
+    time = new TimePickerDialog.OnTimeSetListener() {
+      @Override
+      public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        myCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        myCalendar.set(Calendar.MINUTE, minute);
+        updateTimeLabel();
+        }
+    };
+
+    dateButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+            MainActivity.this,
+            date,
+            myCalendar.get(Calendar.YEAR),
+            myCalendar.get(Calendar.MONTH),
+            myCalendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+      }
+    });
+    timeButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        //new TimePicker
+        new TimePickerDialog(
+          MainActivity.this,
+          time,
+          myCalendar.get(Calendar.HOUR_OF_DAY),
+          myCalendar.get(Calendar.MINUTE), true).show();
+      }
+    });
+  }
+
+  private void updateDateLabel() {
+    String myFormat = "MM/dd/yy"; // In which you need put here
+    SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+    dateButton.setText(sdf.format(myCalendar.getTime()));
+    // dateButton.setFreezesText(true);
+  }
+
+  private void updateTimeLabel() {
+    String myFormat = "H:mm";
+    SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+    timeButton.setText(sdf.format(myCalendar.getTime()));
+  }
+
+
+  private long saveData(
+      int year,
+      int month,
+      int day,
+      int hour,
+      int minute,
+      String from,
+      String to) {
+    FeedReaderDbHelper feedReaderDbHelper = new FeedReaderDbHelper((Context)this);
+
+    ContentValues content = new ContentValues();
+    content.put(FeedReaderContract.FeedEntry.COLUMN_NAME_FROM, from);
+    content.put(FeedReaderContract.FeedEntry.COLUMN_NAME_TO, to);
+    content.put(FeedReaderContract.FeedEntry.COLUMN_NAME_YEAR, year);
+    content.put(FeedReaderContract.FeedEntry.COLUMN_NAME_MONTH, month);
+    content.put(FeedReaderContract.FeedEntry.COLUMN_NAME_DAY, day);
+    content.put(FeedReaderContract.FeedEntry.COLUMN_NAME_HOUR, hour);
+    content.put(FeedReaderContract.FeedEntry.COLUMN_NAME_MINUTE, minute);
+
+    long newRowId;
+    SQLiteDatabase db = feedReaderDbHelper.getWritableDatabase();
+    newRowId = db.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, content);
+
+    Log.d("Mitfahren", String.valueOf(newRowId));
+    return newRowId;
+  }
+
+  /**
+   * Save the data of the current calendar.
+   * @param view Current View.
+   */
+  public void saveData(View view) {
+    EditText vonText = (EditText) findViewById(R.id.editTextVon);
+    EditText nachText = (EditText) findViewById(R.id.editTextNach);
+
+    String fromString = vonText.getText().toString();
+    String toString = nachText.getText().toString();
+
+    if (fromString != "" && toString != "") {
+      int year = myCalendar.get(Calendar.YEAR);
+      int month = myCalendar.get(Calendar.MONTH);
+      int day = myCalendar.get(Calendar.DAY_OF_MONTH);
+      int hour = myCalendar.get(Calendar.HOUR_OF_DAY);
+      int minute = myCalendar.get(Calendar.MINUTE);
+      saveData(year, month, day, hour, minute, fromString, toString);
+      loadData();
     }
+  }
 
-    private String loadData(){
-        FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper((Context)this);
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+  private String loadData() {
+    FeedReaderDbHelper feedReaderDbHelper = new FeedReaderDbHelper((Context)this);
+    SQLiteDatabase db = feedReaderDbHelper.getReadableDatabase();
 
-        String[] projection = {
-                FeedEntry.COLUMN_NAME_ENTRY_ID,
-                FeedEntry.COLUMN_NAME_FROM,
-                FeedEntry.COLUMN_NAME_TO,
-                FeedEntry.COLUMN_NAME_YEAR,
-                FeedEntry.COLUMN_NAME_MONTH,
-                FeedEntry.COLUMN_NAME_DAY,
-                FeedEntry.COLUMN_NAME_HOUR,
-                FeedEntry.COLUMN_NAME_MINUTE
-        };
+    String[] projection = {
+      FeedReaderContract.FeedEntry.COLUMN_NAME_ENTRY_ID,
+      FeedReaderContract.FeedEntry.COLUMN_NAME_FROM,
+      FeedReaderContract.FeedEntry.COLUMN_NAME_TO,
+      FeedReaderContract.FeedEntry.COLUMN_NAME_YEAR,
+      FeedReaderContract.FeedEntry.COLUMN_NAME_MONTH,
+      FeedReaderContract.FeedEntry.COLUMN_NAME_DAY,
+      FeedReaderContract.FeedEntry.COLUMN_NAME_HOUR,
+      FeedReaderContract.FeedEntry.COLUMN_NAME_MINUTE
+    };
+    String sortOrder =
+                FeedReaderContract.FeedEntry.COLUMN_NAME_FROM + " DESC";
 
-        String sortOrder =
-                FeedEntry.COLUMN_NAME_FROM + " DESC";
+    Cursor cursor = db.query(
+        FeedReaderContract.FeedEntry.TABLE_NAME,
+        projection,
+        null,
+        null,
+        null,
+        null,
+        sortOrder);
+    cursor.moveToFirst();
+    String test = cursor.getString(
+        cursor.getColumnIndex(FeedReaderContract.FeedEntry.COLUMN_NAME_DAY)
+    );
+    return test;
+  }
 
-        Cursor c = db.query(FeedEntry.TABLE_NAME, projection, null, null, null, null, sortOrder);
-        c.moveToFirst();
-        String test = c.getString(c.getColumnIndex(FeedEntry.COLUMN_NAME_DAY));
-        return test;
-    }
-
-    private long saveData(int year, int month, int day, int hour, int minute, String from, String to){
-        FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper((Context)this);
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
-        ContentValues content = new ContentValues();
-        content.put(FeedEntry.COLUMN_NAME_FROM, from);
-        content.put(FeedEntry.COLUMN_NAME_TO, to);
-        content.put(FeedEntry.COLUMN_NAME_YEAR, year);
-        content.put(FeedEntry.COLUMN_NAME_MONTH, month);
-        content.put(FeedEntry.COLUMN_NAME_DAY, day);
-        content.put(FeedEntry.COLUMN_NAME_HOUR, hour);
-        content.put(FeedEntry.COLUMN_NAME_MINUTE, minute);
-
-        long newRowId;
-        newRowId = db.insert(FeedEntry.TABLE_NAME, null, content);
-
-        return newRowId;
-    }
 
 }
